81d796f5871294fa71109f8d20ff80f3505a1046
Ionhedblom <ionysoshedblom@gmail.com>
2021-01-10 20:24:15 +0100

    Modifier

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ import './assets/styles/styles.css';
 function App() {
   return (
     <div className='App'>
-      <h1 className='nomobile'>This app is only for desktop.</h1>
-      <div className='onlydesktop'>
-        <Board />
-      </div>
+      <Board />
     </div>
   );
 }
